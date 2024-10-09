af090f36220a1ac78eefe24fd045a7c62046de67
Williamarvin <williamarvin111@gmail.com>
2024-10-09 16:16:52 +0800

    Added comments

--- main.py
+++ main.py
@@ -6,17 +6,23 @@ def wordle(wordList: List[str] = [], maxtries: int = 6) -> bool:
     if wordList == []:
         return False
 
+    # choose random answer from wordList
     answer = random.choice(wordList)
 
     for round in range(maxtries):
 
         wordInput = input("Please input a word to the wordle: ")
+
+        # if correct, then return true
         if wordInput == answer:
             return True
 
+        # if word input is in the list
         if wordInput.lower() in wordList:
+            # check if hit, present or miss
             pass
 
+    # Failed so return false
     return False
 
 
@@ -26,6 +32,7 @@ if __name__ == "__main__":
 
     success = wordle(wordList=words, maxtries=maxtries)
 
+    # check if success
     if success:
         print("wordle success")
 
