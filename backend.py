5d16fcbe66c573b0ef41dc10cf75811849519615
Williamarvin <williamarvin111@gmail.com>
2024-10-09 21:23:20 +0800

    Moved wordle to another file, for server side

---
+++ backend.py
@@ -0,0 +1,67 @@
+from typing import List
+from multiprocessing import Process
+import random
+import zmq
+
+
+class wordle(Process):
+    # initialise wordle class
+    def __init__(self, wordList: List[str] = [], maxtries: int = 6):
+        Process.__init__(self)
+        context = zmq.Context()
+        self.socket = context.socket(zmq.PAIR)
+        self.socket.connect("tcp://localhost:5555")
+
+        self.wordList = wordList
+        self.maxtries = maxtries
+
+    # Draw board based on user input
+    def drawBoard(self, indexList: List[int] = []):
+        board = []
+
+        # HIT 0 = 0
+        # Present 1 = ?
+        # Miss 2 = _
+        for index in indexList:
+            # if hit
+            if index == 0:
+                board.append('0')
+            # if present
+            elif index == 1:
+                board.append('?')
+            # if miss
+            elif index == 2:
+                board.append('_')
+
+        print(''.join(board))
+
+    # check if the word input is hit, present or miss compared to answer
+    def checker(self, wordInput, answer) -> List[int]:
+        output = []
+        # check if hit, present or miss
+        for index in range(5):
+
+            # if hit
+            if wordInput[index] == answer[index]:
+                output.append(0)
+
+            # if present
+            elif wordInput[index] in answer:
+                output.append(1)
+
+            # if miss
+            else:
+                output.append(2)
+
+        return output
+
+    # Start wordle game
+    def run(self) -> bool:
+        if self.wordList == []:
+            return False
+
+        # choose random answer from wordList
+        answer = random.choice(self.wordList)
+
+        for i in range(self.maxtries):
+            pass
